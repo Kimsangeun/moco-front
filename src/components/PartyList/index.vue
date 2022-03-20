687775d13d08b0d1244255c5be874cd4a8c5e0a5
김상은 <tkddmsrla@collabi.co.kr>
2022-03-20 17:24:39 +0900

    home list 작업중

--- src/components/PartyList/index.vue
+++ src/components/PartyList/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="party-list-wrap">
     <div v-for="item in list" :key="item.id" class="party-card">
-      <el-card>
+      <el-card @click="gotoDetail(item.id)">
         <div slot="header" class="party-card-header">
+          <div class="title">{{ item.title }}</div>
           <div class="status">
             <el-tag class="status-icon" :class="`status-${item.status}`" :type="statusType(item.status)" size="mini" effect="dark">{{ statusName(item.status) }}</el-tag>
           </div>
-          <div class="title">{{ item.title }}</div>
         </div>
         <div class="list-item-wrap">
           <div class="list-item">
@@ -43,12 +43,12 @@ export default {
         {
           id: 0,
           status: 'a',
-          title: '파이썬 프로젝트 하시죠~파이썬 프로젝트 하시죠~...',
+          title: '파이썬 프로젝트 하시죠~',
           party_date_str: '2022.04.02 (토)',
           user_num: '1',
           max_num: '6',
           time_str: '11:00 ~ 22:00',
-          location: '서울시 은평구',
+          location: '경기도 은평구',
           tag_list: [
             'python',
             'mysql',
@@ -118,12 +118,15 @@ export default {
   },
   methods: {
     statusName(value) {
-      const name = value === 'a' ? '모집중' : value === 'b' ? '진행중' : '진행완료'
+      const name = value === 'a' ? '모집중' : value === 'b' ? '진행중' : '종료'
       return name
     },
     statusType(value) {
       const type = value === 'a' ? 'success' : value === 'b' ? 'warning' : 'danger'
       return type
+    },
+    gotoDetail(id) {
+      console.log(id)
     }
   }
 }
